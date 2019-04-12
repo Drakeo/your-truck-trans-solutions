b4957574f07a991407f2714f91628f226746e83d
drakeo <flats_fixed@flatsfixedbicycles.com>
2019-04-12 04:57:48 -0500

    work android script

--- android/install.sh
+++ android/install.sh
@@ -11,9 +11,19 @@ ACCT=$(whoami)
 DPBX=Dropbox
 
 
-if [ -f $HOME/YTTS/bin ]; then
+
 read -p ' This will overwrite your old installation Y/N : ' decides
+ 
+  
+if [ "$decides" = "N" ]; then
+   echo Ok $ACCT
+     exit
 fi
+ 
+  
+
+if [ -d $HOME/YTTS/bin ]; then
+read -p ' This will overwrite your old installation Y/N : ' decides
 
 if [ "$decides" = "n" ]; then
    echo good day $ACCT
@@ -26,15 +36,15 @@ if [ "$decides" = "N" ]; then
 fi
 rm -rf $HOME/YTTS
 mkdir -p $HOME/YTTS/bin
-cp  YTTS-admin  YTTS-customer-sort  YTTS-payroll  YTTS-rsync  YTTS-setup  YTTS-sort  YTTS-split  $HOME/YTTS/bin/
+cp -rf YTTS-* $HOME/YTTS/bin/
 cp -rf customers $HOME/YTTS/
 cd  $HOME/YTTS/bin/
 sh YTTS-setup
 else
-read -p ' This will overwrite your old installation Y/N : ' install
 mkdir -p $HOME/YTTS/bin
-cp  YTTS-admin  YTTS-customer-sort  YTTS-payroll  YTTS-rsync  YTTS-setup  YTTS-sort  YTTS-split   $HOME/YTTS/bin/
-cp  customers $HOME/YTTS/
+cp -rf YTTS-* $HOME/YTTS/bin/
+cp -rf icons  $HOME/YTTS/
+cp -rf customers $HOME/YTTS/
 cd  $HOME/YTTS/bin/
 sh YTTS-setup
 fi
